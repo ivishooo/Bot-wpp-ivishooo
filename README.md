49e741ba471bbc81c688b40bf76b52d0a9e0f8aa
Iván Otero <iotero@genit.com.ar>
2022-04-11 16:30:23 -0300

    Bot funcionando

---
+++ README.md
@@ -0,0 +1,24 @@
+<h1 align="center">Welcome to tupa-bot-wpp 👋</h1>
+<div align="center">
+  <p>
+    <img alt="Version" src="https://img.shields.io/badge/version-1.0-blue.svg?cacheSeconds=2592000" />
+  </p>
+</div>
+
+> A Whatsapp bot to transform images, videos and gifs into sticker and send it back to sender.
+
+## Install
+
+```sh
+yarn dev
+```
+
+## Author
+
+👤 **Erick Alexandre**
+
+* Github: [@alexandreerick](https://github.com/alexandreerick)
+* LinkedIn: [@erick-alexandre-83007b139](https://linkedin.com/in/erick-alexandre-83007b139)
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ src/index.ts
@@ -0,0 +1,87 @@
+import { create, decryptMedia, Client } from '@open-wa/wa-automate';
+
+create({
+  sessionId: 'Ivishooo BOT',
+}).then((client) => start(client));
+
+function start(client: Client) {
+  client.onMessage(async message => {
+
+    // SEND VIDEO AND GIF AS STICKER
+    if(message.text == "!labura"){
+      if (message.type === 'video' && !message.isGroupMsg) {
+        const mediaData = await decryptMedia(message);
+        const imageBase64 = `data:${message.mimetype};base64,${mediaData.toString(
+          'base64'
+        )}`;
+        const response = await client.sendMp4AsSticker(
+          message.sender.id,
+          imageBase64,
+          {},
+          {
+            author: 'IVISHOOO BOT',
+            pack: 'EL BOT DE IVISHOOO',
+            keepScale: true,
+          }
+        );
+        console.log('RESPONSE VIDEO', response);
+      }
+
+      // SEND IMAGE AS STICKER
+      if (message.mimetype && message.type !== 'video' && message.type !== 'sticker' && !message.isGroupMsg) {
+        const mediaData = await decryptMedia(message);
+        const imageBase64 = `data:${message.mimetype};base64,${mediaData.toString(
+          'base64'
+        )}`;
+        const response = await client.sendImageAsSticker(
+          message.sender.id,
+          imageBase64,
+          {
+            author: 'IVISHOOO BOT',
+            pack: 'EL BOT DE IVISHOOO',
+            keepScale: true
+          }
+        );
+        console.log('RESPONSE PRIVATE MESSAGE', response);
+      }
+
+      // CHECK IF MESSAGE IS A GROUP MESSAGE
+      if (message.isGroupMsg) {
+        if (message.type === 'video') {
+          const mediaData = await decryptMedia(message);
+          const imageBase64 = `data:${message.mimetype};base64,${mediaData.toString(
+            'base64'
+          )}`;
+          const response = await client.sendMp4AsSticker(
+            message.from,
+            imageBase64,
+            {},
+            {
+              author: 'IVISHOOO BOT',
+            pack: 'EL BOT DE IVISHOOO',
+              keepScale: true
+            }
+          );
+          console.log('RESPONSE VIDEO', response);
+        }
+
+        if (message.mimetype && message.type !== 'video' && message.type !== 'sticker') {
+          const mediaData = await decryptMedia(message);
+          const imageBase64 = `data:${message.mimetype};base64,${mediaData.toString(
+            'base64'
+          )}`;
+          const response = await client.sendImageAsSticker(
+            message.from,
+            imageBase64,
+            {
+              author: 'IVISHOOO BOT',
+              pack: 'EL BOT DE IVISHOOO',
+              keepScale: true,
+            }
+          );
+          console.log('RESPONSE IMAGE GROUP', response);
+        }
+      }
+    }
+  });
+}
